b8460acc2e04e1d8cd6e8b31d90b531f3bbce900
unknown <leonhardleopold@gmail.com>
2021-06-17 23:32:04 +0200

    Cleaned up and added comments to method 1 & 2

--- method1.py
+++ method1.py
@@ -8,8 +8,17 @@ from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 import joblib
 
-def linearsvc(load):
 
+###########################################################
+# Main LinearSVC method. Loads the dataset, applies
+# preprocessing, trains the model, saves the model
+# parameters and prints the  classification report
+# Parameters:
+#   - load (Bool if the weights should be loaded)
+# Returns
+#   -
+def linearsvc(load):
+    # Loading dataset
     data = []
     g = gzip.open("Software.json.gz", 'r')
     print("Loading dataset ...")
@@ -19,28 +28,38 @@ def linearsvc(load):
     print("The dataset used has ", len(data), "entries! Of this dataset", N, "entries are used to train the model.")
     data = data[:N]
 
+    # creating pandas dataframe with two needed columns
     df = pd.DataFrame.from_records(data)[['overall', 'reviewText']]
     df.fillna("", inplace=True)
+    # vectorized the input texts
     tfidf = TfidfVectorizer(max_features=20000, ngram_range=(1, 5), analyzer='char')
     print("Preprocessing ...")
     x = tfidf.fit_transform(df['reviewText'])
     y = df['overall']
+
+    # splits the training set into training and test set
     print("splitting dataset ...")
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
 
+    # creating the model
     print("training model ...")
     clf = LinearSVC(C = 20, class_weight="balanced", verbose=1)
 
+    # loads the model if wanted
     if load:
         print("\nLoading previous model weights:\n")
         clf = joblib.load("weights/linearSVC.sav")
 
+    # trains the model
     clf.fit(X_train, y_train)
 
+    # predicting the the ratings on the test set
     y_pred = clf.predict(X_test)
 
+    # printing the classification report
     print(classification_report(y_test, y_pred))
 
+    # testing the model with 3 examples (positive, negative, neutral review)
     print("Testing model:\n ")
     x = "I really like this book. It is one of the best I have read."
     vec = tfidf.transform([x])
@@ -52,6 +71,7 @@ def linearsvc(load):
     vec = tfidf.transform([x])
     print("'" + x + "' got the rating: ", clf.predict(vec)[0])
 
+    # saving the model weights
     print("\n\nSaving model weights ...")
     joblib.dump(clf, "weights/linearSVC.sav")
 
